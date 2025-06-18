bcff67303481cf529201a2ccb4a1305016f312ed
Sunil Kumar <sunil.kksdk@gmail.com>
2025-06-18 10:58:43 +0530

    implementing MVC Architecture in BastaStoreage

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,8 +1,5 @@
 import {MongoClient} from "mongodb";
 
-// export const client = new MongoClient(
-//   "mongodb://sunil:sunil@localhost:27017/BastaStorage"
-// );
 export const client = new MongoClient(
   "mongodb://sunil:sunil@localhost:27017/BastaStorage?replicaSet=myReplicaSetSunil"
 );
@@ -11,6 +8,5 @@ export default async function connectDB() {
   await client.connect();
   const db = client.db("BastaStorage");
   console.log("Database connected");
-
   return db;
 }
