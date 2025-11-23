e371cb237a954f5a114c8e94b182cd33e52e4da4
Sunil Kumar <sunil.kksdk@gmail.com>
2025-11-23 11:12:39 +0530

    update redis file

--- Backend/database/redis.js
+++ Backend/database/redis.js
@@ -7,6 +7,8 @@ const rediclient = createClient({
     url: process.env.REDIS_URL,
 });
 
+redis.on("error", (err) => console.log("Redis Error:", err));
+
 await rediclient.connect()
 
 export default rediclient;
