712240b63cdde81f9139ca2c0bb40288c9b44ad6
Sunil Kumar <sunil.kksdk@gmail.com>
2026-02-04 23:32:40 +0530

    fix: move cloudfront signer variables inside function to fix url expiration and env loading issues

--- Backend/services/cloudFront.js
+++ Backend/services/cloudFront.js
@@ -1,12 +1,14 @@
 import { getSignedUrl as getCloudFrontSignedUrl } from "@aws-sdk/cloudfront-signer";
 
-const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY.replace(/\\n/g, '\n');
-const keyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
-const dateLessThan = new Date(Date.now() + 60 * 60 * 1000); // 1 hour from now
-const distributionName = `https://${process.env.CLOUDFRONT_DOMAIN_NAME}`;
+
 
 export function createCloudFrontSignedUrl({ fileKey, fileName, download }) {
     try {
+        const dateLessThan = new Date(Date.now() + 60 * 60 * 1000).toISOString(); // 1 hour from now
+
+        const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY.replace(/\\n/g, '\n');
+        const keyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
+        const distributionName = `https://${process.env.CLOUDFRONT_DOMAIN_NAME}`;
 
         const url = getCloudFrontSignedUrl({
             url: `${distributionName}/${fileKey}?response-content-disposition=${encodeURIComponent(`${download ? "attachment" : "inline"}; filename="${fileName}"`)}`,
