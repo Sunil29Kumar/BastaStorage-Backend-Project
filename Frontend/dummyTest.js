6170d3e79305b0543355861bbf20e606d293bbc5
Sunil Kumar <sunil.kksdk@gmail.com>
2026-02-07 10:49:57 +0530

    Add dummyTest

---
+++ Frontend/dummyTest.js
@@ -0,0 +1,12 @@
+const shoudTestPass = true;
+
+if (shoudTestPass) {
+    console.log("Test pass");
+    
+    process.exit(0);
+}
+
+else {
+    console.log("Test failed");
+    process.exit(1);
+}
