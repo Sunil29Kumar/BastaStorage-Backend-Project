25d69462720528c8d2ee8bb49298b6daecc58b49
Sunil Kumar <sunil.kksdk@gmail.com>
2026-04-22 14:39:00 +0530

    4th Testing GitHub Action for Frontend deployment

--- Frontend/src/landing/LandingPage.jsx
+++ Frontend/src/landing/LandingPage.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import Features from "./Features";
 import Footer from "./Footer";
 import DashboardImage from "./DashboardImage";
-import FAQ from "./FAQ.JSX";
+import FAQ from "./FAQ";
 import DumiPlans from "./DumiPlans";
 
 const LandingPage = () => {
